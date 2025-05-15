462dd3fd80fbf471cceef02bd260965fdbc406dd
develepe564205690 <mynameisnotarlo@gmail.com>
2025-05-15 14:13:01 -0500

    Add files via upload

--- hehehaha/hello.py
+++ hehehaha/hello.py
@@ -1,15 +1,44 @@
-from flask import Flask
+from flask import Flask, render_template, request
 import random
+
+
+
 app = Flask(__name__)
 app.config['DEBUG'] = True
+app.config['TEMPLATES_AUTO_RELOAD'] = True
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        color = request.form.get('color', '')
+        luck_num = request.form.get('luck_num', '')
+        fav_class = request.form.get('fav_class', '')
+        best_pix = request.form.get('best_pix', '')
+        return f"Your favorites - Color: {color}, Lucky Number: {luck_num}, Class: {fav_class}, Pixar Movie: {best_pix}"
+    return render_template("favorite_template.html")
+
+@app.route('/thanks', methods=['GET', 'POST'])
+def thanks():
+    try:
+        values = {
+            'name': request.form.get('name', 'Friend'),
+            'gift': request.form.get('gift', 'present'),
+            'verb': request.form.get('verb', 'playing'),
+            'noun': request.form.get('noun', 'person'),
+            'closing_word': request.form.get('closing_word', 'Sincerely'),
+            'author': request.form.get('author', 'Me')
+        }
+        return render_template("tynote.html", **values)
+    except Exception as e:
+        return f"Error: {str(e)}"
+
+@app.route('/hello')
 def hello():
    message = "Hello, Flask!"
    return message
 
 
-@app.route('/hello')
+@app.route('/rand')
 def here():
    page = """
       <h1>Here's a random number: {0}</h1>
@@ -19,6 +48,8 @@ def here():
    """
    num = random.randint(1, 25)
    return page.format(num)
+
+
 
 if __name__ == '__main__':
    app.run()
